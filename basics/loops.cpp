80dd157ee7f8c0fece82ebf9ced9ee94ca0a06d9
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-01-28 13:09:54 +0530

    Create loops.cpp

---
+++ basics/loops.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+#include <bits/stdc++.h> //includes all the libraries of c++
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        cout<<"coding is fun!"<<endl;
+    }
+    
+    int j=1;
+    while(j<=5)
+    {
+        cout<<"bye"<<endl;
+        j=j+1;
+    }
+    
+    int z=5;
+    do{
+        cout<<"see you later"<<endl;
+        z=z-1;
+    }while(z>0);
+}
