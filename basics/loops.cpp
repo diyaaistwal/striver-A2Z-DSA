954f97d781dd6e7ed4f7000d8affe1c52b93b011
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-01-28 13:15:36 +0530

    Update loops.cpp

--- basics/loops.cpp
+++ basics/loops.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
+    /*
+    ### **Loops in C++**
+
+Loops allow repetitive execution of a block of code until a condition is met.
+
+---
+
+    #### **1. `for` Loop**
+- Repeats a block of code a fixed number of times.
+- Ideal for scenarios with a known range of iterations.
+
+    #### **2. `while` Loop**
+- Repeats a block of code as long as the condition is true.
+- Suitable for scenarios where the number of iterations is unknown beforehand.
+
+    #### **3. `do-while` Loop**
+- Similar to `while`, but guarantees at least one execution of the code block since the condition is checked after execution.
+
+---
+
+   ### **Key Difference:**
+- `for`: Use for fixed iterations.
+- `while`/`do-while`: Use for condition-based repetition.
+    */ 
+    
     for(int i=0;i<5;i++)
     {
         cout<<"coding is fun!"<<endl;
