2fcd74a016994f70b1c06b4abc855ca6bc6f2e70
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-01-28 20:20:07 +0530

    Update deque.cpp

--- basics/STL/deque.cpp
+++ basics/STL/deque.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
-#include<deque>
+#include <vector>
+
 using namespace std;
 int main()
 {
     //double ended queue-dynamic arrays, non contiguous memory, random access
-    deque<int> dq;
-    dq.push_back(10);
-    dq.push_back(20);
-    dq.push_back(30);
-    dq.push_back(40);
-    dq.push_front(-10);
-    dq.push_front(-20);
-    for(int val:dq)
+    pair<int, int> p={2,4};
+    
+    cout<<p.first<<endl;
+    cout<<p.second<<endl;
+    
+    pair<int, pair<char, string>> pp={1,{'d',"diya"}};
+    
+    cout<<pp.first<<endl;
+    cout<<pp.second.second<<endl;
+    
+    vector<pair<int,int>> v={{1,2},{2,3}};
+    
+    v.push_back({4,5});
+    v.emplace_back(4,5); //creates in place objects
+    for(pair<int,int> key:v)
     {
-        cout<<val<<" ";
+        cout<<key.first<<" "<<key.second<<",";
     }
     
-    cout<<endl<<"value at index 3: "<<dq[3];
     return 0;
 }
