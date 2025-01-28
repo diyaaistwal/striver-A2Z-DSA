eb61abcc5eaccd6750404d674fecd013e620c6c5
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-01-28 13:36:57 +0530

    Create passbyvalue&ref.cpp

---
+++ basics/passbyvalue&ref.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+#include <bits/stdc++.h> //includes all the libraries of c++
+using namespace std;
+
+
+void append(int num)
+{
+    num=num+50;
+    cout<<"updated number using function:"<<num<<endl;
+}
+
+void append_ref(int &num)
+{
+    num=num+50;
+    cout<<"updated number using function:"<<num<<endl;
+}
+
+int main()
+{
+    int a;
+    cout<<"enter the number";
+    cin>>a;
+    append(a); //pass by value
+    cout<<"number in main,ie,original:"<<a<<endl; 
+    append_ref(a); //pass by reference
+    cout<<"number in main,ie,by reference:"<<a<<endl; 
+}    
+  
+  
