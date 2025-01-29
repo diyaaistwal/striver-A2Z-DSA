a98fd23adb212156a948983631234cfef5c89597
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-01-29 20:47:52 +0530

    Update code.cpp

--- basic_math/code.cpp
+++ basic_math/code.cpp
@@ -55,3 +55,60 @@ bool armstrongNumber(int n) {
         
         
     }
+
+4. sum of divisors:
+https://www.geeksforgeeks.org/problems/sum-of-all-divisors-from-1-to-n4738/1
+int sumOfDivisors(int n) {
+        // Write Your Code here
+        int i,j;
+        int sum;
+        int total_sum=0;
+        for(i=1;i<=n;i++)
+        {
+            sum=0;
+            for(j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                sum=sum+j;
+            }
+            total_sum+=sum;
+        }
+        return total_sum;
+    }
+
+5. minimum jumps:
+https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+int minJumps(vector<int>& arr) {
+        int n = arr.size();
+
+    // If the array has one or no elements, no jump is needed
+    if (n <= 1) return 0;
+
+    // If the first element is 0, we can't move forward
+    if (arr[0] == 0) return -1;
+
+    // If the first element is larger than or equal to the last index, we can directly reach the end
+    if (arr[0] >= n - 1) return 1;
+
+    int jumps = 1;           // Start with 1 jump (since we already jump at least once)
+    int farthest = arr[0];   // The farthest we can reach initially
+    int current_end = arr[0]; // The end of the current jump range
+    
+    for (int i = 1; i < n; i++) {
+        // Update the farthest we can reach
+        farthest = max(farthest, i + arr[i]);
+        
+        // If we have reached the end of the current jump range
+        if (i == current_end) {
+            jumps++; // We need to make another jump
+            current_end = farthest; // Update the range to the farthest point
+            
+            // If the farthest point can reach or exceed the last index, return jumps
+            if (current_end >= n - 1) return jumps;
+        }
+    }
+    
+    return -1; // If we can't reach the end of the array
+    }
+
+6. 
