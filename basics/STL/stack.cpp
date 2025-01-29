b68913a30d6307b718db77d4f44182e2c584c649
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-01-29 12:41:13 +0530

    Create stack.cpp

---
+++ basics/STL/stack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+int main()
+{
+    //push,emplace,pop,top,size,empty,swap; LIFO
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    
+    stack<int> s2;
+    s2.swap(s); //elements of s2 and s interchanged
+    
+    cout<<"top of stack s2: "<<s2.top()<<endl; //display top element
+    
+    while(!s.empty())
+    {
+        cout<<"top of stack: "<<s.top()<<endl;
+        s.pop();
+    }
+    
+    cout<<"size of s: "<<s.size()<<endl;
+    cout<<"size of s2: "<<s2.size()<<endl;
+    return 0;
+}
